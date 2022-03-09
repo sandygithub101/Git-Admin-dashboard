30ff75d0eb7f3340b014f3d70f8a581e652f874e
sandygithub101 <kumar.sandeep101162@gmail.com>
2022-03-09 18:41:44 +0530

    Initial commit

---
+++ plugins/chart.js/my_chart.js
@@ -0,0 +1,55 @@
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['America', 'Russia', 'Autralia', 'Canada', 'Uk', 'India'],
+        datasets: [{
+            label: 'Traffics By Country',
+            data: [1200, 1900, 300, 500, 200, 3000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+           
+        }]
+    },
+    options: {
+       responsive: true,
+    }
+});
+
+
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1208, 1921, 3012, 5046, 2451, 3722, 1000, 1540, 1845, 21003, 47850, 62001],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+           
+        }]
+    },
+    options: {
+       responsive: true,
+    }
+});
